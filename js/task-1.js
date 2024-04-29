a9cd20e5399500361b3aa6771897fdde828f34f3
Verona0609 <dikaya.nika15@gmail.com>
2024-04-29 17:25:32 +0200

    hw7(task1,2)

--- js/task-1.js
+++ js/task-1.js
@@ -1,33 +1,28 @@
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
 
-};
+    const ulElem = document.querySelector("#categories");
+  
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+    const itemEl = document.querySelectorAll(".item");
+
+    console.log(`Number of categories : ${itemEl.length}`);
+  
+        const items = document.querySelectorAll('#categories .item');
+   
+items.forEach(item => {
+   
+    const category = item.querySelector("h2").textContent;
+    const elements = item.querySelectorAll("ul li").length;
+
+
+console.log(`Category: ${category}`)
+console.log(`Elements: ${elements}`)
+
+});
+
+
+
+
+
 
 
 
